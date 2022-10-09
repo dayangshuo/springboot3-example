bdd507ddeb4fc11587b6256655a9c2f85b71cfa7
dayang <dayang9927@163.com>
2022-10-09 17:22:45 +0800

    springboot3-web

---
+++ springboot3-web/src/main/java/cn/dayangshuo/springboot/WebApplication.java
@@ -0,0 +1,14 @@
+package cn.dayangshuo.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author DAYANG
+ */
+@SpringBootApplication
+public class WebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
+}
---
+++ springboot3-web/src/main/java/cn/dayangshuo/springboot/controller/TestController.java
@@ -0,0 +1,21 @@
+package cn.dayangshuo.springboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author DAYANG
+ */
+@RestController
+public class TestController {
+
+    /**
+     * <a href="http://localhost:8080/hello?name=dayang">点击我</a>
+     * @param name param
+     * @return string
+     */
+    @GetMapping("/hello")
+    public String hello(String name) {
+        return String.join(",", "hello", name);
+    }
+}
